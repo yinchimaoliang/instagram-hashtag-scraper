119a447a4d50b93dd05159ef33328a221594b90d
yinchimaoliang <liyinhao0413@qq.com>
2019-05-23 20:17:49 +0800

    finish hashtag name

--- MAIN.py
+++ MAIN.py
@@ -1,20 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+URL_BASE = 'https://top-hashtags.com/instagram/'
+PAGE_NUM = 10
+
+
 class main():
+    def __init__(self,url_base):
+        self.url_base = url_base
+        self.hashtags = []
+        self.urls = [url_base]
+        for i in range(PAGE_NUM):
+            self.urls.append(self.url_base + str((i + 1) * 100 + 1) + '/')
 
-url_base = 'https://top-hashtags.com/hashtag/'
-web_data = requests.get(url_base)
-soup = BeautifulSoup(web_data.text,'lxml')
-# hashtags = soup.select('ul > li > div > ul > li > div > div > a')
-# hashtags = soup.select('ul > li > div')
-# print(hashtags)
-# t = soup.select('div.entry-content > ul.tht.no-bullet > li > div.row.align-top > div > a')
-t = soup.select('div.entry-content > ul.tht.no-bullet ')
-t2 = t[0].select('li > div.row.align-top > div.tht-tag.small-7.medium-9.columns')
-for i in t2:
-    print(i.text)
+        print(self.urls)
 
+        # print(self.urls)
+    def getOnePage(self,url):
+        web_data = requests.get(url)
+        soup = BeautifulSoup(web_data.text,'lxml')
+        t = soup.find_all(name='ul', attrs={'class': 'tht no-bullet'})
+        # t2 = t[0].select('li > div.row.align-top > div.tht-tag.small-7.medium-9.columns')
+        t2 = t[0].select('li > div.row.align-top > div.tht-tag.small-7.medium-9.columns')
+        for i in t2:
+            self.hashtags.append(i.text[1:])
+
+    def getAllHashtags(self):
+        for url in self.urls:
+            self.getOnePage(url)
+
+
+
+
+    def mainFunc(self):
+        self.getAllHashtags()
+
+
+
+if __name__ == '__main__':
+    t = main(URL_BASE)
+    t.mainFunc()
 
 # print(soup)
 # titles = soup.select('//*[@id="post-69"]/div[1]/ul[1]/li[1]/div/div[2]/a)
